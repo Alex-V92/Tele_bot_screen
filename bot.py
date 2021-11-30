3cd8a0b5def26a980522effcfc928d0c4b4caa77
alex_v92 <Nfyxbr_11>
2021-11-30 16:44:04 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,18 @@
+import config
+import telebot
+import pyautogui
+from os.path import exists, isfile
+from os import remove
+from time import gmtime, strftime
+
+bot = telebot.TeleBot(config.TOKEN)
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли
+    if isfile('test.png'):
+        remove('test.png')
+    bot.send_message(message.chat.id, 'Отправка скриншота...')
+    bot.send_message(message.chat.id, strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+    pyautogui.screenshot( 'test' +'.png')
+    bot.send_photo(message.chat.id,open('test.png','rb'))
+
+bot.polling(none_stop=True)
